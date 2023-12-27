b88e7fbaa354a45555bc3989bd932b4a770d85e2
vaibhavpharate <62934219+vaibhavpharate@users.noreply.github.com>
2023-12-27 17:31:52 +0530

    Update database_funcs.py

--- database_funcs.py
+++ database_funcs.py
@@ -12,3 +12,14 @@ def get_connection(host,port,user,passord,database):
     except Exception as e:
         print("\n\n---------------------Connection Failed")
         print(f"{e}")
+
+def get_ci_ct_map(db_connection):
+    df = pd.read_sql_query("SELECT * FROM td_satellite.ct_ci_map",db_connection)
+    df = df.loc[:,['ct_flag','new_ci']]
+    ct_ci_map = {}
+    ct_flag = list(df['ct_flag'])
+    new_ci = list(df['new_ci'])
+
+    for x in range(len(ct_flag)):
+        ct_ci_map[ct_flag[x]] = new_ci[x]
+    return ct_ci_map
